8c201d8954d7fb5ab4d85a63fb46a52861557dfa
Christoph Eberl <office@cebra-webdesign.at>
2015-06-13 12:03:10 +0200

    outcomment old service - test successfull

--- app/js/services/service.js
+++ app/js/services/service.js
@@ -3,61 +3,61 @@ angular.module('chatapp.services', [])
  *
  */
 
-	.factory('RoomService', ['$interval', '$filter', '$http',  function($interval, $filter, $http){
-
-		let rooms = {
-			data: null
-		};
-
-		let roomdata = [];
-
-		// Call to get all Rooms via the Mongoose API
-		let getRooms = function getRooms(){
-
-			//GET Request
-
-			$http({
-				method: 'GET',
-				url: '/api/room'
-			})
-			.success(function(data, status, headers, config) {
-				rooms.data = data;
-				for(let i = 0; i < rooms.data.length; i++){
-					roomdata.push(rooms.data[i].name);
-				}
-			})
-			.error(function(data, status, headers, config) {
-				console.log("error");
-				console.log(status);
-			});
-		};
-
-
-		// Call to insert a new Room into the database via Mongoose API
-		let createRoom = function createRoom(roomName){
-
-			//POST Request
-
-			console.log(roomName);
-			return $http.post('/api/room', roomName);
-		};
-
-		// Call to delete a single Room in the database via Mongoose API
-		let deleteRoom = function deleteRoom(id){
-
-			//DELETE Request
-
-			return $http.delete('/api/room/' + id);
-		};
-
-		return {
-				getRooms : getRooms,
-				createRoom: createRoom,
-				deleteRoom: deleteRoom,
-				roomdata: roomdata
-    };
-
-	}])
+	// .factory('RoomService', ['$interval', '$filter', '$http',  function($interval, $filter, $http){
+	//
+	// 	let rooms = {
+	// 		data: null
+	// 	};
+	//
+	// 	let roomdata = [];
+	//
+	// 	// Call to get all Rooms via the Mongoose API
+	// 	let getRooms = function getRooms(){
+	//
+	// 		//GET Request
+	//
+	// 		$http({
+	// 			method: 'GET',
+	// 			url: '/api/room'
+	// 		})
+	// 		.success(function(data, status, headers, config) {
+	// 			rooms.data = data;
+	// 			for(let i = 0; i < rooms.data.length; i++){
+	// 				roomdata.push(rooms.data[i].name);
+	// 			}
+	// 		})
+	// 		.error(function(data, status, headers, config) {
+	// 			console.log("error");
+	// 			console.log(status);
+	// 		});
+	// 	};
+	//
+	//
+	// 	// Call to insert a new Room into the database via Mongoose API
+	// 	let createRoom = function createRoom(roomName){
+	//
+	// 		//POST Request
+	//
+	// 		console.log(roomName);
+	// 		return $http.post('/api/room', roomName);
+	// 	};
+	//
+	// 	// Call to delete a single Room in the database via Mongoose API
+	// 	let deleteRoom = function deleteRoom(id){
+	//
+	// 		//DELETE Request
+	//
+	// 		return $http.delete('/api/room/' + id);
+	// 	};
+	//
+	// 	return {
+	// 			getRooms : getRooms,
+	// 			createRoom: createRoom,
+	// 			deleteRoom: deleteRoom,
+	// 			roomdata: roomdata
+  //   };
+	//
+	// }])
 
 	.factory('UserService', ['$interval', '$filter', '$http',  function($interval, $filter, $http){
 		return {
@@ -122,4 +122,3 @@ angular.module('chatapp.services', [])
 				}
 		};
 	}]);
-	
