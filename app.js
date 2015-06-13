5f0b00e51152671909f1ea9ec6db749fe6044359
Bernhard Zweimüller <bernhard.zweimueller@gmail.com>
2015-06-13 21:57:26 +0200

    merge
    app.js

--- app.js
+++ app.js
@@ -86,19 +86,6 @@ io.sockets.on('connection', function (socket) {
   });
 
 
-<<<<<<< Updated upstream
-
-
-
-
-
-
-  // when the client emits 'sendchat', this listens and executes
-  socket.on('sendchat', function (data) {
-    // we tell the client to execute 'updatechat' with 2 parameters
-    io.sockets.in(socket.room).emit('updatechat', socket.username, data);
-  });
-=======
 	// when the client emits 'sendchat', this listens and executes
 	socket.on('sendchat', function (data) {
 		// we tell the client to execute 'updatechat' with 2 parameters
@@ -107,7 +94,6 @@ io.sockets.on('connection', function (socket) {
 
 	// when the client emits 'adduser', this listens and executes
 	socket.on('adduser', function(username, roomName){
->>>>>>> Stashed changes
 
   // when the client emits 'adduser', this listens and executes
   socket.on('adduser', function(username, roomName){
